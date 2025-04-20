bbbfbbb27bc7858c0a8b614180cbad35882308b7
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-04-20 21:13:48 +0100

    Add dfmt

--- flake.nix
+++ flake.nix
@@ -1,33 +1,56 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-    packages.x86_64-linux = rec {
-      default = dscanner;
-      dscanner = pkgs.buildDubPackage rec {
-        pname = "dscanner";
-        version = "0.15.2";
+    in
+    {
+      packages.x86_64-linux = {
+        dscanner = pkgs.buildDubPackage rec {
+          pname = "dscanner";
+          version = "0.15.2";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "dlang-community";
-          repo = "D-Scanner";
-          rev = "v${version}";
-          hash = "sha256-7lZhYlK07VWpSRnzawJ2RL69/U/AH/qPyQY4VfbnVn4=";
+          src = pkgs.fetchFromGitHub {
+            owner = "dlang-community";
+            repo = "D-Scanner";
+            rev = "v${version}";
+            hash = "sha256-7lZhYlK07VWpSRnzawJ2RL69/U/AH/qPyQY4VfbnVn4=";
+          };
+
+          dubLock = ./dscanner-dub-lock.json;
+
+          doCheck = true;
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 bin/dscanner -t $out/bin
+            runHook postInstall
+          '';
         };
 
-        dubLock = ./dub-lock.json;
+        dfmt = pkgs.buildDubPackage rec {
+          pname = "dfmt";
+          version = "0.15.2";
 
-        doCheck = true;
+          src = pkgs.fetchFromGitHub {
+            owner = "dlang-community";
+            repo = "dfmt";
+            rev = "v${version}";
+            hash = "sha256-QjmYPIQFs+91jB1sdaFoenfWt5TLXyEJauSSHP2fd+M=";
+          };
 
-        installPhase = ''
-          runHook preInstall
-          install -Dm755 bin/dscanner -t $out/bin
-          runHook postInstall
-        '';
+          dubLock = ./dfmt-dub-lock.json;
+
+          doCheck = true;
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 bin/dfmt -t $out/bin
+            runHook postInstall
+          '';
+        };
       };
     };
-  };
 }
